9d6a9c020eec57ab5bdedddfa14e91c666eb04fe
Rajeev Ranjan <rjranjan2112@gmail.com>
2026-08-13 18:06:31 +0530

    Intalled next. Setup done

---
+++ client/app/lib/api.ts
@@ -0,0 +1,41 @@
+const API_URL =
+  process.env.NEXT_PUBLIC_API_URL ||
+  "http://127.0.0.1:8000";
+
+export async function apiFetch<T>(
+  path: string,
+  options?: RequestInit,
+): Promise<T> {
+  const response = await fetch(
+    `${API_URL}${path}`,
+    {
+      ...options,
+      headers: {
+        "Content-Type": "application/json",
+        ...options?.headers,
+      },
+    },
+  );
+
+  if (!response.ok) {
+    let message = "Something went wrong";
+
+    try {
+      const data = await response.json();
+
+      if (typeof data.detail === "string") {
+        message = data.detail;
+      }
+    } catch(err: any){
+      console.error(err.message);
+    }
+
+    throw new Error(message);
+  }
+
+  if (response.status === 204) {
+    return undefined as T;
+  }
+
+  return response.json();
+}
