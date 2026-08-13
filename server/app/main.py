8940c9f79dfcb3c2bf077359d2c8be6563d84006
Rajeev Ranjan <rjranjan2112@gmail.com>
2026-08-13 17:23:26 +0530

    Register responses and statistics routers

--- server/app/main.py
+++ server/app/main.py
@@ -3,6 +3,9 @@ from app.database import Base, engine
 from app.models import (Answer, Form, Question, QuestionOption,Response, User)
 from app.routers.forms import router as forms_router
 from app.routers.questions import router as questions_router
+from app.routers.public import router as public_router
+from app.routers.statistics import router as statistics_router
+from app.routers.responses import router as response_router
 
 Base.metadata.create_all(bind=engine)
 
@@ -14,6 +17,9 @@ app = FastAPI(
 
 app.include_router(forms_router)
 app.include_router(questions_router)
+app.include_router(public_router)
+app.include_router(response_router)
+app.include_router(statistics_router)
 
 @app.get("/api/health")
 def health_check():
